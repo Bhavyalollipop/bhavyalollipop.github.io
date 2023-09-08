3160e12f38efee36d44065bc3072867c40a1f475
Bhavyalollipop <bhavyaparameswaran@gmail.com>
2023-09-08 19:46:35 +0530

    push 2023 latest change

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
-const gulp = require('gulp')
-const purgecss = require('gulp-purgecss')
-gulp.task('purgecss', () => {
-    return gulp.src(['assets/vendor/bootstrap/css/bootstrap.min.css','node_modules/@fortawesome/fontawesome-free/css/all.min.css'])
-        .pipe(purgecss({
-            content: ['index.html']
-        }))
-        .pipe(gulp.dest('assets/css'))
-})
+const gulp = require("gulp");
+const purgecss = require("gulp-purgecss");
+gulp.task("purgecss", () => {
+  return gulp
+    .src([
+      "assets/vendor/bootstrap/css/bootstrap.min.css",
+      "node_modules/font-awesome/css/font-awesome.min.css",
+    ])
+    .pipe(
+      purgecss({
+        content: ["index.html"],
+      })
+    )
+    .pipe(gulp.dest("assets/css"));
+});
